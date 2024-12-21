661628b9d8154bc37a0fc9f67cbcb7c022a6a0d6
Josue Soto <sotoherrerajosue@gmail.com>
2024-12-20 23:07:02 -0600

    project experience of linuxtica website

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Linuxtica Website"
+              description="Responsive website of the company Linuxtica, participating in the development of it"
+              ghLink="***********"
+              demoLink="https://www.linuxtica.com/"              
             />
           </Col>
 
